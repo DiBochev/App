a56ebadc10b908eb7834899c12d75e33ce363eb3
fgg@mail.bg <fgg@mail.bg>
2014-11-12 14:03:28 +0200

    FirstAndroid

---
+++ app/src/main/java/com/contact/contact/MainActivity.java
@@ -0,0 +1,73 @@
+package com.contact.contact;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements SensorEventListener{
+
+    Sensor accelerometer;
+    SensorManager sm;
+    TextView X;
+    TextView Y;
+    TextView Z;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        X = (TextView) findViewById(R.id.X);
+        Y = (TextView) findViewById(R.id.Y);
+        Z = (TextView) findViewById(R.id.Z);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        X.setText("X:" + event.values[0]);
+        Y.setText("Y:" + event.values[1]);
+        Z.setText("Z:" + event.values[2]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
